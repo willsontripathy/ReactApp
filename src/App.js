426f44ea0107fabd186b5f8c4263338d847f0ba8
WILLSON <willson.tripathy@gmail.com>
2021-07-09 17:19:41 +0530

    useState hook demo.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  const [appState, setAppState] =  React.useState("Willson");
+  const [count,setCount] = React.useState(1);
+
+  const btnClick = () =>{
+    const name = (count % 2 == 1) ? "Will Tripathy": "Willson";
+    setAppState(name);
+    setCount(count +1);
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{count}</h2>
+      <h4>Hello {appState}</h4>
+      <button type="button" onClick={btnClick}>Change Name</button>
     </div>
   );
 }
